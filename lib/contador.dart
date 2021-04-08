2ad62384ab0690c31baf6fbf94dab63c3c67d299
jcbnetto <netinho.bruno@outlook.com>
2021-04-07 21:55:56 -0300

    Aula 3
    Atualizando Tela

---
+++ lib/contador.dart
@@ -0,0 +1,48 @@
+// Construtor do Widget mutável (Stateful):
+import 'package:flutter/material.dart';
+
+class Contador extends StatefulWidget {
+  @override
+  _ContadorState createState() => _ContadorState();
+}
+
+// Conteúdo do Widget mutável:
+class _ContadorState extends State<Contador> {
+  // variáveis;
+  var contador = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contador"),
+      ),
+      body: Center(
+        child: GestureDetector(
+          child: Text(
+            "Cliques: $contador",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.black, fontSize: 40.0),
+          ),
+          onTap: () {
+            // Eventos que acontecerão quando clicar no item:
+            //contador++;
+            //print(contador);
+            // Comando para definir novo estado da tela:
+            setState(() {
+              contador++;
+            });
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          setState(() {
+            contador++;
+          });
+        },
+        icon: Icon(Icons.add),
+        label: Text("Somar"),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'contador.dart';
+
 void main() {
   runApp(MyApp());
 }   
 
 class MyApp extends StatelessWidget{
   // Receber um parâmetro
-  
   @override
   Widget build(BuildContext context) {
   return MaterialApp(
-    theme: ThemeData.light(),
-    home: Inicio());
-
-  }
-}
-class Inicio extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-       child: Center(
-         child: Text(
-          "Netto",
-            textDirection: TextDirection.ltr,
-         style: TextStyle(color: Colors.amber, fontSize: 20.0),
-         ),
-       ),
+    theme: ThemeData(primarySwatch: Colors.amber),
+    home: Contador(),
     );
   }
-
 }
