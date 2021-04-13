51cd65da242c2def21f244e810b06cd8b060f914
jcbnetto <netinho.bruno@outlook.com>
2021-04-12 22:21:08 -0300

    Aula 5
    endereço das imagens

--- lib/cardapio.dart
+++ lib/cardapio.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 import 'componentes.dart';
@@ -12,18 +11,45 @@ class _CardapioState extends State<Cardapio> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Cardápio")),
-         body: Container(
+        appBar: AppBar(title: Text("Cardápio")),
+        body: Container(
+          // Definir altura e largura do container:
           width: double.infinity,
+          height: double.infinity,
           child: Column(
-          
-           mainAxisAlignment: MainAxisAlignment.center,
-           crossAxisAlignment: CrossAxisAlignment.center,
-           children: [
-             Text("Text 1"), Text("Text 2"), Text("Text 3"),],
-         ),
-       ),
-    );
+            children: [
+              Image(
+                image: AssetImage('assets/images/habilideburguer.jpg'),
+                // alterar opubspec:
+              ),
+              Titulo(
+                txt: "Habilideburguer",
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Preco(txt: "R\$ 35,90"),
+                  ElevatedButton(
+                    onPressed: () => null,
+                    child: Text("Comprar"),
+                  )
+                ],
+              ),
+              Titulo(
+                txt: "Descrição",
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text(
+                    "Lanche saborosão com salada fresquinha e queijo prato"),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: null,
+          label: Text("Carrinho"),
+          icon: Icon(Icons.add),
+        ));
   }
 }
--- lib/componentes.dart
+++ lib/componentes.dart
@@ -1,16 +1,49 @@
 import 'package:flutter/material.dart';
 
+// estilo do Preço:
+class Preco extends StatelessWidget {
+  final String txt;
+
+  const Preco({Key key, this.txt}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      txt,
+      style: TextStyle(
+          fontSize: 20, color: Color(0xFF00AF00), fontWeight: FontWeight.bold),
+    );
+  }
+}
+
+// Estilo do Titulo:
+class Titulo extends StatelessWidget {
+  final String txt;
+
+  const Titulo({Key key, this.txt}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Text(
+        txt,
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
+
 class Sara extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-         padding: const EdgeInsets.only(top: 10),
-         child: Image(
-           // Definir a largura da imagem:
-           // Double.infinity = 100%
-           width: double.infinity,
-           image: NetworkImage('https://conteudo.imguol.com.br/c/entretenimento/bb/2021/04/04/bbb-21-sarah-no-faustao-1617570138763_v2_450x337.jpg'),
-         ),
+      padding: const EdgeInsets.only(top: 10),
+      child: Image(
+        // Definir a largura da imagem:
+        // Double.infinity = 100%
+        width: double.infinity,
+        image: NetworkImage(
+            'https://conteudo.imguol.com.br/c/entretenimento/bb/2021/04/04/bbb-21-sarah-no-faustao-1617570138763_v2_450x337.jpg'),
+      ),
     );
   }
 }
